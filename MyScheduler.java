eec30b946d900acd89bc85bece259a6d6fab71e3
Quade Leonard <quade.leonard@yahoo.com>
2024-03-27 14:38:44 -0400

    Added all setup BS

--- MyScheduler.java
+++ MyScheduler.java
@@ -1,3 +1,45 @@
+// Josh Rowe and Quade Leonard
+
+import java.util.*;
+import java.util.concurrent.*;
+
 public class MyScheduler {
-    
+    private int numJobs;
+    private String property; // four possible options 'avg wait', 'max wait', 'combined', 'deadlines'
+    // 'avg wait' use shortest job first
+    LinkedBlcokingQueue<Job> outgoing;
+    LinkedBlcokingQueue<Job> incoming;
+
+    public MyScheduler(int numJobs, String property) {
+        this.numJobs = numJobs;
+        this.property = property;
+        this.outgoing = getOutgoingQueue();
+        this.incoming = getIncomingQueue();
+        
+    }
+
+    public LinkedBlockingQueue<Job> getOutgoingQueue() {
+        LinkedBlockingQueue<Job> outgoing = new LinkedBlockingQueue(numJobs);
+        return outgoing;
+    }
+
+    public LinkedBlockingQueue<Job> getIncomingQueue() {
+        LinkedBlcokingQueue<Job> incoming = new LinkedBlockingQueue(numJobs);
+        return incoming;
+    }
+
+    public void run() {
+        switch(property){
+            case "avg wait":
+            break;
+            case "max wait":
+            break;
+            case "combined":
+            break;
+            case "deadlines":
+            break;
+            default:
+            break;
+        }
+    }
 }
